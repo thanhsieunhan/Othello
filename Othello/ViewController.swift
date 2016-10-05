ae43d989038cc0b76b0db773a78cd896c3daf79f
thanhsieunhan <thanhlh.2101@gmail.com>
2016-10-05 09:39:11 +0700

    Initial Commit

---
+++ Othello/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Othello
+//
+//  Created by Lê Hà Thành on 10/5/16.
+//  Copyright © 2016 Lê Hà Thành. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
